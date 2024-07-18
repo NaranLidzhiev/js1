d2247c9d62f7f8dd28984928d8e2bdaf4028a2b6
NaranLidzhiev <lidzhievnaran483@gmail.com>
2024-07-18 11:21:11 +0300

    promise

--- practive.js
+++ practive.js
@@ -1,24 +1,3 @@
-'use strict';
-
-// Создаётся объект promise
-let promise = new Promise((resolve, reject) => {
-
-   setTimeout(() => {
-      // переведёт промис в состояние fulfilled с результатом "result"
-      resolve("result");
-   }, 1000);
-
-});
-
-// promise.then навешивает обработчики на успешный результат или ошибку
-promise
-    .then(
-        result => {
-           // первая функция-обработчик - запустится при вызове resolve
-           console.log("Fulfilled: " + result); // result - аргумент resolve
-        },
-        error => {
-           // вторая функция - запустится при вызове reject
-           console.log("Rejected: " + error); // error - аргумент reject
-        }
-    );
+let str = "Это моя винтовка. Таких винтовок много но это - моя"
+console.log(str.match(/моя/gi))
+console.log(str.match(/моя/i))
