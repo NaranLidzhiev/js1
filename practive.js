4c9d05c7c59288e64251337f665e364d06a98d4a
NaranLidzhiev <lidzhievnaran483@gmail.com>
2024-07-18 11:35:15 +0300

    promise1

--- practive.js
+++ practive.js
@@ -1,3 +1,14 @@
 let str = "Это моя винтовка. Таких винтовок много но это - моя"
 console.log(str.match(/моя/gi))
 console.log(str.match(/моя/i))
+console.log(str.match(/КАСКАД/gi))
+
+beta = str.replace(/винтовка/gi, 'кошка')
+console.log(beta)
+
+if (/много/i.test(str)){
+   console.log("True")
+} else{
+   console.log("False")
+}
+
